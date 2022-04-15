4ca1cf24b56bf70cef6383631390760dc7b370a2
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-15 18:30:14 +0200

    con la funcion upper ya funciona correctamente

--- Clases/ejercicio2.py
+++ Clases/ejercicio2.py
@@ -3,9 +3,10 @@ import time
 class Entrada:
     def entrada_estandar():
         mensaje=str(input("Qué mensaje quieres escribir\n"))
+        mensajefinal= mensaje.upper()
         print("Se va a generar el fichero de texto en unos instantes...")
         fichero = open("Ejercicio2.txt", "a")
-        fichero.write(mensaje)
+        fichero.write(mensajefinal)
         fichero.close()
         time.sleep (10) #se borra tras 10 segundos
         os.remove("Ejercicio2.txt")
