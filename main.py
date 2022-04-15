2044b94af11477d010bc66d4283fb58ebe87b971
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-15 18:19:58 +0200

    creado el main y ejrcicios

---
+++ main.py
@@ -0,0 +1,12 @@
+
+def eleccion_ejercicio():
+    variable = int(input("\nSeleccione que ejercicio desea ejecutar: (1-3)"))
+    if variable == 1:
+        from Clases import ejercicio1
+    if variable == 2:
+        from Clases import ejercicio2
+    if variable == 3:
+        from Clases import ejercicio3
+    else:
+        eleccion_ejercicio()
+eleccion_ejercicio()
