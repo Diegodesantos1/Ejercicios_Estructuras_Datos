948e2b8ea79ecedf462f4cd441ecd7c0428b4384
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-15 18:49:41 +0200

    añadidos los getter y puestos los rounds

--- Clases/ejercicio3.py
+++ Clases/ejercicio3.py
@@ -4,20 +4,28 @@ class Productos:
         self.entretenimiento = entretenimiento
         self.servicio = servicio
         self.precio_inicial = precio_inicial
+    def alimentario(self):
+        return self.alimentario
+    def entretenimiento(self):
+        return self.entretenimiento
+    def servicio(self):
+        return self.servicio
+    def precio_inicial(self):
+        return self.precio_inicial
     def iva ():
         precio_inicial = 100
         eleccion = int(input(("Bienvenido a su tienda online, que desea comprar con sus 100€: \n 1: Alimentos \n 2: Servicio \n 3: Entretenimiento\n\n")))
         if eleccion == 1:
-            precio_final = precio_inicial * 1.04
+            precio_final = round(precio_inicial * 1.04)
             print(f"La compra de ha realizado correctamente, le ha costado {precio_final}€")
             exit()
         if eleccion == 2:
-            precio_final = precio_inicial * 1.21
+            precio_final = round(precio_inicial * 1.21)
             print(f"La compra de ha realizado correctamente, le ha costado {precio_final}€")
             exit()
         if eleccion == 3:
-            precio_final = precio_inicial * 1.1
-            print(f"La compra de ha realizado correctamente, le ha costado {precio_final}e")
+            precio_final =round( precio_inicial * 1.1)
+            print(f"La compra de ha realizado correctamente, le ha costado {precio_final}€")
             exit()
         else:
             print("Opción no válida")
